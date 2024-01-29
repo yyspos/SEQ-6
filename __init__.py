9d402c04cae911aa9b29f1cd0021fddb6cd3797e
bstocker <boris.stocker@gmail.com>
2024-01-29 14:46:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-import sqlite3
+import mysql.connector
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -21,17 +21,12 @@ def hello_world_fr():
 # Création d'une nouvelle route pour la lecture de la BDD
 @app.route('/lecture/')
 def ReadBDD():
-    conn=sqlite3.connect('database.db')
-    #return "<h2>Route lecture</h2>"
-    #conn = get_db_connection()
-    #posts = conn.execute('SELECT * FROM livres').fetchall()
-    #conn.close()
-
-    # Convertit la liste de livre en un format JSON
-    # json_posts = [{'id': post['id'], 'title': post['title'], 'content': post['auteur']} for post in posts]
-
-    # Renvoie la réponse JSON
-    # return jsonify(posts=json_posts)
+    conn = mysql.connector.connect(
+    host='localhost',
+    user='boris',
+    password='Scooter90%1',
+    database='boris_BDD'
+    )
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
