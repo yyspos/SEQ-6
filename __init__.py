44fde4eddda945b5dbcf33e8de4f4df3d4e446fa
bstocker <boris.stocker@gmail.com>
2024-01-29 15:36:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ def ReadBDD():
     conn.close()
 
     # Construisez un dictionnaire avec les données
-    json_posts = [{'colonne1': row[0], 'colonne2': row[1]} for row in data]
+    json_posts = [{'colonne1': row['id'] for row in data]
     # Ajoutez d'autres clés au dictionnaire en fonction de la structure de votre base de données
 
     # Renvoyez les données au format JSON
