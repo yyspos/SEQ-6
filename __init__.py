690768507be1ec8cd480055a8f1b66e2f40efc45
bstocker <boris.stocker@gmail.com>
2024-01-29 16:33:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, render_template
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 import sqlite3
