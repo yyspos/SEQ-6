9f07848a762a9f1fe5002c956dc0d6fa2fcbe73a
bstocker <boris.stocker@gmail.com>
2024-02-01 10:09:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -59,6 +59,7 @@ def meteo():
     response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
         temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
