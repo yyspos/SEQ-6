0ed3a9d02562103da978cfb23f523cce766cb72d
bstocker <boris.stocker@gmail.com>
2024-02-09 12:06:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,33 @@ import sqlite3
 
 app = Flask(__name__)                                                                                                                  
 
+# Fonction pour créer une clé "authentifie" dans la session utilisateur
+def est_authentifie():
+    return session.get('authentifie')
+
+@app.route('/lecture')
+def lecture():
+    if not est_authentifie():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentification'))
+
+  # Si l'utilisateur est authentifié
+    return "<h2>Bravo, vous êtes authentifié</h2>"
+
+@app.route('/authentification', methods=['GET', 'POST'])
+def authentification():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+    return render_template('formulaire_authentification.html', error=False)
+
 
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
