6895e071a9becd5707c28be6ae17db10351f4f59
bstocker <boris.stocker@gmail.com>
2024-02-09 12:48:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 import sqlite3
 
 app = Flask(__name__)                                                                                                                  
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
