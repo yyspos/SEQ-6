9fddc9581a2c469b646c395b1656ed8d7cd878d5
bstocker <boris.stocker@gmail.com>
2024-03-08 15:36:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,6 +11,10 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
 @app.route('/lecture')
 def lecture():
     if not est_authentifie():
@@ -42,7 +46,6 @@ def Readfiche(post_id):
     cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
     data = cursor.fetchall()
     conn.close()
-    
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
@@ -55,69 +58,6 @@ def ReadBDD():
     conn.close()
     return render_template('read_data.html', data=data)
 
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-
-@app.route('/fr/')
-def hello_world_fr():
-    return "<h2>Bonjour tout le monde 2!</h2>"
-
-# Création d'une nouvelle route pour la lecture de la BDD
-@app.route('/lecture/')
-def ReadBDD2():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM livres;')
-    data = cursor.fetchall()
-    conn.close()
-
-   # Construisez dynamiquement une chaîne de modèle HTML
-    template_string = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Bibliotheque</title>
-    </head>
-    <body>
-        <h1>Contenu de la base de données</h1>
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>DATE</th>
-                <th>TITRE</th>
-                <th>AUTEUR</th>
-            </tr>
-            {% for row in data %}
-            <tr>
-                <td>{{ row[0] }}</td>
-                <td>{{ row[1] }}</td>
-                <td>{{ row[2] }}</td>
-                <td>{{ row[3] }}</td>
-            </tr>
-            {% endfor %}
-        </table>
-    </body>
-    </html>
-    """
-    return render_template_string(template_string, data=data)
-
-@app.route('/paris/')
-def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
 
                                                                                                                                        
 if __name__ == "__main__":
