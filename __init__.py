5d2a479e7874a13e9e190fc03d3cd419a87f30ba
Yann <137297146+yyspos@users.noreply.github.com>
2025-01-23 10:03:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def accueil():
     return render_template('index.html')
+
+@app.route('/catalogue')
+def catalogue():
+    return render_template('catalogue.html')
+
+@app.route('/emprunts')
+def emprunts():
+    return render_template('emprunts.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
